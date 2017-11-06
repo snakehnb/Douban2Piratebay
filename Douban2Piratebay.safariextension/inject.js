7acec5c924e7d70ebe4b60c6c52f4c7969cc208d
snakehnb <huangnaibo@gmail.com>
2017-11-06 22:13:24 +0800

    InitSafariEdition
    Create a basic safari extion

---
+++ Douban2Piratebay.safariextension/inject.js
@@ -0,0 +1,40 @@
+console.log("hello world");
+
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function addLink(fragment, text, href){
+  let a = imdblink.cloneNode(true);
+  a.textContent = text;
+  a.href = href;
+  fragment.appendChild(a);
+}
+
+var links = document.querySelectorAll('#info > a');
+
+var imdblink = null;
+var imdbRe = new RegExp("tt[0-9]{4,}");
+for (var i=0; i<links.length; i++) {
+  if(imdbRe.test(links[i].textContent)) {
+    imdblink = links[i];
+    break;
+  }
+}
+
+
+if (imdblink !== null) {
+  console.log("imdb link found");
+  var imdbindex = imdblink.innerText;
+  var fragment = document.createDocumentFragment();
+  var br = document.createElement("br");
+  var span = imdblink.previousElementSibling.cloneNode(false);
+  span.textContent = "资源：";
+  fragment.appendChild(br);
+  fragment.appendChild(span);
+  addLink(fragment, "TPB ", 'https://thepiratebay.org/search/' + imdbindex);
+  addLink(fragment, "RARGB ", 'https://rarbgmirror.com/torrents.php?imdb=' + imdbindex);
+  addLink(fragment, "HD湾 ", 'http://www.hdwan.net/?s=' + imdbindex);
+  addLink(fragment, "电影天堂 ", 'http://www.btrr.net/?s=' + imdbindex);
+  insertAfter(fragment, imdblink);
+}
